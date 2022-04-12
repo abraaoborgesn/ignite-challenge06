bb0dff3a8eb628ad8a5c0cdfa18411ced66cd808
Abraão <abraao.gt2396@hotmail.com>
2022-04-12 19:55:45 -0300

    commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,33 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+    colors: {
+        background: "#1D1D1D",
+        dark: {
+            "black": "#000000",
+            "headingsAndText": "#47585B",
+            "info": "#999999",
+            "info-50": "rgba(153, 153, 153, 0.5)"
+        },
+        light: {
+            "white": "#FFFFFF",
+            "headingsAndText": "#F5F8FA",
+            "info": "#DADADA"
+        },
+        Highlight: "#FFBA08",
+        Highlight_50: "rgba(255, 186, 8, 0.5)",
+
+    },
+    fonts: {
+        heading: 'Poppins',
+        body: 'Poppins'
+    },
+    styles: {
+        global: {
+            body: {
+                bg: 'background',
+                color: 'light.headingsAndText'
+            }
+        }
+    }
+})
